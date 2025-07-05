eacbebaed8214bf06ad1684abbac5a2facb72662
andrewref <161936928+andrewref@users.noreply.github.com>
2025-07-05 09:16:18 +0300

    base class

--- src/com/fawry/model/Product.java
+++ src/com/fawry/model/Product.java
@@ -1,5 +1,22 @@
 package com.fawry.model;
 
 public abstract class Product {
-
+	protected String name;
+	protected double price;
+	protected int quantity;
+ public Product(String name,double price,int quantity) {
+	 this.name= name;
+	 this.price=price;
+	 this.quantity=quantity;
+ }
+ 
+ public String getName()     { return name; }
+ public double getPrice()    { return price; }
+ public int getQuantity()    { return quantity; }
+ 
+ public void reduceQuantity(int q) {
+     if (q<=quantity) {
+         quantity -= q;
+     }
+ }
 }
